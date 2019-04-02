baeaefe108fa084bd26bef417732c4702c3e0d11
JunyoungChoi <jcjy0311@naver.com>
2019-04-02 12:39:51 +0900

    Update baekjoon_1358_C.c

--- baekjoon_1358_C.c
+++ baekjoon_1358_C.c
@@ -11,36 +11,12 @@ int main(void)
 	scanf("%f %f %f %f %f", &W, &H, &X, &Y, &P);
 	for (unsigned char i = 0; i < P; i++) {
 		scanf("%d %d", &p[i].x, &p[i].y);
-		if ((p[i].x >= X - (H / 2) && p[i].x <= X + W + (H / 2)) && (p[i].y <= Y + H && p[i].y >= Y)) {
-			if (p[i].x < X) {
-				if (sqrt(pow(X - p[i].x, 2) + pow((Y + (H / 2) - p[i].y) > 0 ? Y + (H / 2) - p[i].y : p[i].y - Y - (H / 2), 2)) > H / 2) {
-					printf("1 안추가 %f %f %f\n", X - p[i].x, Y + (H / 2) - p[i].y > 0 ? Y + (H / 2) - p[i].y : p[i].y - Y - (H / 2) , sqrt(pow(X - p[i].x, 2) + pow((Y + (H / 2) - p[i].y) > 0 ? Y + (H / 2) - p[i].y : p[i].y - Y - (H / 2), 2)));
-
-				}
-				else {
-					ans++;
-					printf("1 추가 %f %f %f\n", X - p[i].x, Y + (H / 2) - p[i].y > 0 ? Y + (H / 2) - p[i].y : p[i].y - Y - (H / 2), sqrt(pow(X - p[i].x, 2) + pow((Y + (H / 2) - p[i].y) > 0 ? Y + (H / 2) - p[i].y : p[i].y - Y - (H / 2), 2)));
-
-				}
-			}
-			else if (p[i].x > X + W) {
-				if (sqrt(pow(p[i].x - X - W, 2) + pow((Y + (H / 2) - p[i].y) > 0 ? Y + (H / 2) - p[i].y : p[i].y - Y - (H / 2), 2)) > H / 2) {
-					printf("2 안추가 %f %f %f\n", p[i].x - X - W, Y + (H / 2) - p[i].y > 0 ? Y + (H / 2) - p[i].y : p[i].y - Y - (H / 2), sqrt(pow(p[i].x - X - W, 2) + pow((Y + (H / 2) - p[i].y) > 0 ? Y + (H / 2) - p[i].y : p[i].y - Y - (H / 2), 2)));
-
-				}
-				else {
-					ans++;
-					printf("2 추가 %f %f %f\n", p[i].x - X - W, Y + (H / 2) - p[i].y > 0 ? Y + (H / 2) - p[i].y : p[i].y - Y - (H / 2), sqrt(pow(p[i].x - X - W, 2) + pow((Y + (H / 2) - p[i].y) > 0 ? Y + (H / 2) - p[i].y : p[i].y - Y - (H / 2), 2)));
-				}
-			}
-			else {
-				if (p[i].y >= Y && p[i].y <= Y + H) {
-					ans++;
-					printf("3 추가\n");
-				}
-			}
+		if ((p[i].x >= X && p[i].x <= X + W && p[i].y <= Y + H && p[i].y >= Y)) {
+			ans++;
 		}
-		
+		else {
+			sqrt(pow((p[i].x <= X) ? X-p[i].x : (p[i].x - W-X), 2) + pow((p[i].y > Y + H / 2) ? p[i].y - Y - H / 2 : Y + H / 2 - p[i].y, 2)) <= H / 2 ? ans++ : 0;
+		}
 	}
 	printf("%d", ans);
 	return 0;
