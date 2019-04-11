6fd8d6efd9636f84a5b6b789bbcc50fe39bb3b13
JunyoungChoi <jcjy0311@naver.com>
2019-04-11 21:35:53 +0900

    Create baekjoon_2439_C.c

---
+++ baekjoon_2439_C.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main() {
+	int N;
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (i + j >= N - 1)
+				printf("*");
+			else
+				printf(" ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
