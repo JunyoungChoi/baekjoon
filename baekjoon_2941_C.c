6086e3af26806e2f6cc9a0c8cbce7b409069663b
JunyoungChoi <jcjy0311@naver.com>
2019-04-14 01:00:08 +0900

    Create baekjoon_2941_C.c

---
+++ baekjoon_2941_C.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char k[8][4] = { "c=","c-","dz=","d-","lj","nj","s=","z=" };
+	char s[101];
+	char *ptr;
+	int cnt, len, cnt_dz = 0, cnt_z = 0;
+	scanf("%s", s);
+	len = strlen(s);
+	for (int i = 0; i < 8; i++) {
+		cnt = 0;
+		if (strstr(s, k[i])) {
+			ptr = s;
+			while (strstr(ptr, k[i]) != 0) {
+				if (i == 2)
+					cnt_dz++;
+				if (i == 7)
+					cnt_z++;
+				ptr = strstr(ptr, k[i]);
+				cnt++;
+				ptr += 1;
+			}
+			len -= cnt*(strlen(k[i]) - 1);
+		}
+	}
+	printf("%d", len+cnt_dz++);
+	return 0;
+}
