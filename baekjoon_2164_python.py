73acc902e5e4f1ce036bf519f2ee4e5f452191d4
JunyoungChoi <jcjy0311@naver.com>
2019-05-15 18:47:10 +0900

    Update baekjoon_2164_python.py

--- baekjoon_2164_python.py
+++ baekjoon_2164_python.py
@@ -1,6 +1,8 @@
-import sys
-num_list = [x for x in range(1,int(sys.stdin.readline().rstrip())+1)]
-tem=2
-while tem<=len(num_list):
-    num_list = num_list[2:]+num_list[1:2]
-print(num_list[0])
+n = int(input())
+
+m = 1
+while m < n:
+    m *= 2
+m -= (2 * (m - n))
+
+print(m)
