f02ba2edc55efeb8de575f4df9d83d54abc1f857
JunyoungChoi <jcjy0311@naver.com>
2019-05-30 18:35:45 +0900

    Create baekjoon_1074_python.py

---
+++ baekjoon_1074_python.py
@@ -0,0 +1,18 @@
+import sys
+c, a, b = map(int,sys.stdin.readline().rstrip().split())
+an = str(bin(a))[2:]
+bn = str(bin(b))[2:]
+
+lth = 0
+if len(an) > len(bn):
+    lth = len(an)
+    bn = '0'*(lth-len(bn)) + bn
+else:
+    lth = len(bn)
+    an = '0'*(lth-len(an)) + an
+    
+r = ''
+for i in range(lth):
+    r += an[i] + bn[i]
+
+print(int(r,2))
